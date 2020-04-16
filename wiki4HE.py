f8df16f94d9de5bc0876850e87b3ef1d0feee130
bsingh17 <61860973+bsingh17@users.noreply.github.com>
2020-04-16 18:37:41 +0530

    Add files via upload

---
+++ wiki4HE.py
@@ -0,0 +1,45 @@
+import numpy as nop
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset=pd.read_csv('wiki4HE.csv',sep=';')
+dataset.replace(to_replace='?',value='0',inplace=True)
+dataset=dataset.astype(int)
+
+from sklearn.preprocessing import StandardScaler,normalize
+scaler=StandardScaler()
+scaler.fit(dataset)
+x=scaler.fit_transform(dataset)
+x=normalize(x)
+from sklearn.decomposition import PCA
+pca=PCA(n_components=2)
+pca.fit(x)
+x_pca=pca.transform(x)
+
+from sklearn.cluster import KMeans
+wcss=[]
+
+for i in range(1,11):
+    kmeans=KMeans(n_clusters=i,init='k-means++',random_state=0)
+    kmeans.fit(x)
+    wcss.append(kmeans.inertia_)
+    
+plt.figure(figsize=(10,7))
+plt.title('Elbow Method')
+plt.xlabel('number of Clsuters')
+plt.ylabel('WCSS')
+plt.plot(range(1,11),wcss)
+plt.show()
+
+kmeans=KMeans(n_clusters=4,init='k-means++',random_state=0)
+y_kmeans=kmeans.fit_predict(x_pca)
+
+plt.figure(figsize=(10,7))
+plt.title('Clustering')
+plt.scatter(x_pca[y_kmeans==0,0],x_pca[y_kmeans==0,1],s=100,c='blue',label='Cluster1')
+plt.scatter(x_pca[y_kmeans==1,0],x_pca[y_kmeans==1,1],s=100,c='pink',label='Cluster2')
+plt.scatter(x_pca[y_kmeans==2,0],x_pca[y_kmeans==2,1],s=100,c='magenta',label='Cluster3')
+plt.scatter(x_pca[y_kmeans==3,0],x_pca[y_kmeans==3,1],s=100,c='red',label='Cluster4')
+plt.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],s=400,c='green',label='Centroid')
+plt.legend()
+plt.show()
+
